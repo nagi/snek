e65c1a34d4c63675121282b760671ef69917676d
Andrew Nagi <andrew.nagi@gmail.com>
2020-05-24 13:59:56 +0100

    Animate a board, with snake and food

--- src/snek/core.clj
+++ src/snek/core.clj
@@ -1,4 +1,63 @@
 (ns snek.core)
 
+(def board-height 5)
+(def board-width 5)
+
+(def space \.)
+(def wall  \#)
+(def munchy  \o)
+(def head  \+)
+(def tail  \+)
+
+(defn board-new[height width]
+  (->> space
+       (repeat width)
+       vec
+       (repeat height)
+       vec))
+
+(defn snake-new [[y x] direction]
+  {:position [y x] :direction direction})
+
+(defn snake-move [{:keys [position direction] :as snake}]
+  (let [[y x] position]
+    (condp = direction
+      :north (snake-new [(dec y) x] direction)
+      :south (snake-new [(inc y) x] direction)
+      :west  (snake-new [y (dec x)] direction)
+      :east  (snake-new [y (inc x)] direction)
+      :stopped snake)))
+
+(def game-new
+  {:board (board-new board-height board-width)
+   :snake (snake-new [1 0] :east)
+   :food [2,2]})
+
+(defn paint[{:keys [board snake food], {:keys [position direction]} :snake} ]
+  (let [tiles (for [row (range board-height)]
+                (for [cell (range board-width)]
+                  (cond
+                    (= [row cell] position) head
+                    (= [row cell] food) munchy
+                    :else ((board row) cell))))
+        picture (clojure.string/join "\n"
+                             (map #(apply str %1) tiles))]
+    (println picture)))
+
+(defn start-game[]
+  (loop [turns 5
+         {:keys [board snake food] :as game} game-new]
+    (if (zero? turns)
+      (println "Game Over")
+      (do
+        (paint game)
+        (println "next...")
+        (recur (dec turns)
+               {
+                :snake (snake-move snake)
+                :food food
+                :board board
+                })))))
+
 (defn -main []
-  (println "Hello, World!"))
+  (start-game))
--- test/snek/core_test.clj
+++ test/snek/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [snek.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest snake-movment
+  (testing "move a snake north"
+    (is
+     (= (:position (snake-move (snake-new [5 5] :north))) [4 5])))
+  (testing "move a snake south"
+    (is
+     (= (:position (snake-move (snake-new [5 5] :south))) [6 5])))
+  (testing "move a static west"
+    (is
+     (= (:position (snake-move (snake-new [5 5] :west))) [5 4])))
+  (testing "move a snake east"
+    (is
+     (= (:position (snake-move (snake-new [5 5] :east))) [5 6])))
+  (testing "move a stopped snake"
+    (is
+     (= (:position (snake-move (snake-new [5 5] :stopped))) [5 5]))))

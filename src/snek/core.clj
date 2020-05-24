b8f8327d43034a5e369757049af489839eab90a0
Andrew Nagi <andrew.nagi@gmail.com>
2020-05-24 20:59:51 +0100

    Drive snake, and make him eat food to grow

--- src/snek/core.clj
+++ src/snek/core.clj
@@ -1,13 +1,11 @@
 (ns snek.core)
 
-(def board-height 32)
-(def board-width 32)
+(def board-height 16)
+(def board-width 16)
 
 (def space 0)
-(def wall  \#)
 (def munchy  \X)
 (def head  \+)
-(def tail  \+)
 
 (defn board-new[height width]
   (->> space
@@ -16,22 +14,22 @@
        (repeat height)
        vec))
 
-(defn snake-new [[y x] direction]
-  {:position [y x] :direction direction})
+(defn snake-new [[y x] direction length]
+  {:position [y x] :direction direction :length length})
 
-(defn snake-move-position [{:keys [position direction] :as snake}]
+(defn snake-move-position [{:keys [position direction length] :as snake}]
   (let [[y x] position]
     (condp = direction
-      :north (snake-new [(dec y) x] direction)
-      :south (snake-new [(inc y) x] direction)
-      :west  (snake-new [y (dec x)] direction)
-      :east  (snake-new [y (inc x)] direction)
+      :north (snake-new [(dec y) x] direction length)
+      :south (snake-new [(inc y) x] direction length)
+      :west  (snake-new [y (dec x)] direction length)
+      :east  (snake-new [y (inc x)] direction length)
       :stopped snake)))
 
 (def game-new
   {:board (board-new board-height board-width)
-   :snake (snake-new [10 10] :east)
-   :food [20,20]})
+   :snake (snake-new [5 5] :east 3)
+   :food [10,10]})
 
 (defn paint[{:keys [board snake food], {:keys [position direction]} :snake}]
   (let [tiles (for [row (range board-height)]
@@ -44,31 +42,49 @@
                              (map #(apply str %1) tiles))]
     (println picture)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Control Strats - replace with A.I.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn random-direction[{:keys [direction]}]
   (first
    (shuffle
     (remove (partial = direction) [:north :south, :west, :east]))))
 
+(defn human-player[{:keys [direction]}]
+  (let [ui (read-line)]
+    (println direction)
+    (condp = ui
+      "j" :south
+      "k" :north
+      "h" :west
+      "l" :east)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn snake-turn[snake]
   (Thread/sleep 100)
-  (update snake :direction random-direction))
+  (update snake :direction
+          human-player
+          ;; random-direction
+          ))
 
 (defn board-move
   "Snakes head becomes first part of the tail"
-  [board snake-head]
+  [board {:keys [position length]}]
   (mapv (fn [row] (mapv #(if (and (int? %) (pos? %)) (dec %) %) row))
-          (update-in board snake-head (constantly 3)
-                     )))
+          (update-in board position (constantly length))))
 
 (defn crashed?[board {:keys [position]}]
   (let [cell (get-in board position)]
     (or (nil? cell)
         (pos? cell))))
 
-(let [board [[0 0 0]
-             [0 0 0]
-             [0 0 0]]]
-  (crashed? board (snake-new [-1 -1] :stopped)))
+(defn eat-and-grow[{:keys [position] :as snake} food]
+  (if (= position food)
+    (update snake :length inc)
+    snake))
+
+(defn replace-food[board]
+  [8 8])
 
 (defn start-game[]
   (loop [turns 30
@@ -82,9 +98,9 @@
               (paint game)
               (recur (dec turns)
                      {
-                      :snake (snake-move-position (snake-turn snake))
-                      :food food
-                      :board (board-move board (:position snake))
+                      :snake (snake-move-position (snake-turn (eat-and-grow snake food)))
+                      :food (replace-food board)
+                      :board (board-move board snake)
                       })))))
 
 (defn -main []
